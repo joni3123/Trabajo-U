cd60b1216d0eff1af1fe2f074ce96a3976740432
joni3123 <jd.osorio14@ciaf.edu.co>
2025-08-26 11:16:49 -0500

    Se agregan dos botones, una imagen y un titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
+//Se importan recuros necesarios como imagenes y archvos css
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Esponja from './assets/Esponja.jpg'
+//preparan el componente para mostrar el contador y los botones 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+//Se agregag un titulo y la imagen con su estilo css, tambien se agregan botones y contador
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>´
+        <h1> Aumentar y Disminuir Contador</h1>
+        <img src= {Esponja} alt="EsponjaMafioso" className='imagen-esponja' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="card">       
+        <button onClick={() => setCount(count + 1)}>
+          Incrementar
+        </button>
+        <button onClick={() => setCount(count - 1)}>
+          Disminuir
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          El contador es: {count}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
